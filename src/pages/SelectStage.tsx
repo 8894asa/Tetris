79d4fc973c08f72be2403adf6f1ae697710e72f4
8894asa <r-asada@digeon.co>
2024-06-24 18:26:51 +0900

    画像挿入

--- src/pages/SelectStage.tsx
+++ src/pages/SelectStage.tsx
@@ -1,6 +1,7 @@
+import { Link } from "react-router-dom";
+
 import { QUESTIONS } from "@/const/questions";
 import { url } from "@/const/url";
-import { Link } from "react-router-dom";
 
 export function SelectStage() {
   return (
@@ -9,11 +10,12 @@ export function SelectStage() {
       <div className="grid grid-cols-3 gap-2 mt-24 w-80 mx-auto">
         {QUESTIONS.slice(0, 9).map((question, i) => (
           <Link
+            // eslint-disable-next-line react/no-array-index-key
             key={i}
-            className={`border py-12 px-12 border-gray-500 w-full rounded-md transform transition duration-500 ease-in-out hover:scale-110 active:scale-95`}
+            className="border border-gray-500 w-full rounded-md transform transition duration-500 ease-in-out hover:scale-110 active:scale-95"
             to={`${url.tetrisQuiz}/${question.id}`}
           >
-            <img src={'\pages\image\0.png'} />
+            <img src="/test.jpg" alt="" />
           </Link>
         ))}
       </div>
