d4f01649db3f3ac7796b4e805c2e27469f14ce4d
sakabe005 <sakabe1201@gmail.com>
2024-06-26 01:37:05 +0900

    指摘された問題の修正

--- src/pages/SelectStage.tsx
+++ src/pages/SelectStage.tsx
@@ -1,8 +1,10 @@
-import { QUESTIONS } from "@/const/questions";
-import { url } from "@/const/url";
 import { Link } from "react-router-dom";
 
-export function SelectStage() {
+import { QUESTIONS } from "@/const/questions";
+import { url } from "@/const/url";
+
+
+export function SelectStage0() {
   return (
     <div className="font-bold text-center mt-20">
       <div className="text-4xl">問題選択</div>
@@ -10,11 +12,43 @@ export function SelectStage() {
         {QUESTIONS.slice(0, 9).map((question, i) => (
           <Link
             key={i}
-            className={`border py-12 px-12 border-gray-500 w-full rounded-md transform transition duration-500 ease-in-out hover:scale-110 active:scale-95`}
+            className= "border py-12 px-12 border-gray-500 w-full rounded-md transform transition duration-500 ease-in-out hover:scale-110 active:scale-95"
             to={`${url.tetrisQuiz}/${question.id}`}
-          >
-            <img src={'\pages\image\0.png'} />
-          </Link>
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export function SelectStage1() {
+  return (
+    <div className="font-bold text-center mt-20">
+      <div className="text-4xl">問題選択</div>
+      <div className="grid grid-cols-3 gap-2 mt-24 w-80 mx-auto">
+        {QUESTIONS.slice(0, 9).map((question, i) => (
+          <Link
+            key={i}
+            className="border py-12 px-12 border-gray-500 w-full rounded-md transform transition duration-500 ease-in-out hover:scale-110 active:scale-95"
+            to={`${url.tetrisQuiz}/${question.id}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export function SelectStage2() {
+  return (
+    <div className="font-bold text-center mt-20">
+      <div className="text-4xl">問題選択</div>
+      <div className="grid grid-cols-3 gap-2 mt-24 w-80 mx-auto">
+        {QUESTIONS.slice(0, 9).map((question, i) => (
+          <Link
+            key={i}
+            className= "border py-12 px-12 border-gray-500 w-full rounded-md transform transition duration-500 ease-in-out hover:scale-110 active:scale-95"
+            to={`${url.tetrisQuiz}/${question.id}`}
+          />
         ))}
       </div>
     </div>
