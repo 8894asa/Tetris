5deb336f8b338a14a19f452a784ce462e6f7216c
yaaasuu <anyongw01123@gmail.com>
2024-06-28 00:15:18 +0900

    問題追加id:3~5

--- src/const/questions.ts
+++ src/const/questions.ts
@@ -96,4 +96,105 @@ export const QUESTIONS: Question[] = [
       },
     ],
   },
+  {
+    id: 3,
+    difficulty: "easy",
+    name: "簡単な問題",
+    description: "初心者でもできる簡単な問題です。",
+    initialMinoGrid: [
+      [1, 1, 1, 1, 0, 0, 0, 1, 1, 1], // 3行目
+      [1, 1, 1, 1, 0, 0, 0, 1, 1, 1], // 2行目
+      [1, 1, 1, 1, 0, 0, 1, 1, 1, 1], // 1行目
+    ],
+    currentMinoType: "Z",
+    nextMinoList: ["J"],
+    answer: [
+      {
+        position: { x: 5, y: 1 },
+        rotation: 1,
+      },
+      {
+        position: { x: 4, y: 1 },
+        rotation: 3,
+      },
+    ],
+  },
+  {
+    id: 4,
+    difficulty: "medium",
+    name: "中クラス問題",
+    description: "中クラス問題です。",
+    initialMinoGrid: [
+      [1, 0, 0, 0, 0, 0, 0, 0, 1, 1], // 5行目
+      [1, 1, 0, 0, 0, 0, 1, 1, 1, 1], // 4行目
+      [1, 1, 1, 0, 0, 0, 1, 1, 1, 1], // 3行目
+      [1, 1, 1, 0, 0, 0, 1, 1, 1, 1], // 2行目
+      [1, 1, 1, 0, 0, 0, 1, 1, 1, 1], // 1行目
+    ],
+    currentMinoType: "T",
+    nextMinoList: ["L", "S", "L", "I"],
+    answer: [
+      {
+        position: { x: 4, y: 0 },
+        rotation: 0,
+      },
+      {
+        position: { x: 3, y: 2 },
+        rotation: 1,
+      },
+      {
+        position: { x: 5, y: 2 },
+        rotation: 1,
+      },
+      {
+        position: { x: 5, y: 4 },
+        rotation: 2,
+      },
+      {
+        position: { x: 1, y: 4 },
+        rotation: 0,
+      },
+    ],
+  },
+  {
+    id: 5,
+    difficulty: "medium",
+    name: "中クラス問題",
+    description: "中クラス問題です。",
+    initialMinoGrid: [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 5行目
+      [0, 0, 0, 0, 0, 0, 1, 1, 1, 1], // 4行目
+      [0, 0, 0, 0, 0, 0, 1, 1, 1, 1], // 3行目
+      [1, 1, 0, 0, 1, 1, 1, 1, 1, 1], // 2行目
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 1行目
+    ],
+    currentMinoType: "O",
+    nextMinoList: ["S", "O", "L", "J", "I"],
+    answer: [
+      {
+        position: { x: 2, y: 1 },
+        rotation: 0,
+      },
+      {
+        position: { x: 4, y: 3 },
+        rotation: 1,
+      },
+      {
+        position: { x: 0, y: 2 },
+        rotation: 0,
+      },
+      {
+        position: { x: 5, y: 3 },
+        rotation: 1,
+      },
+      {
+        position: { x: 1, y: 4 },
+        rotation: 2,
+      },
+      {
+        position: { x: 6, y: 4 },
+        rotation: 0,
+      },
+    ],
+  },
 ];
